b3f7a432e70ee14a77004fcf9ab79e1602bf6412
Qiaozhi <ql316@cam.ac.uk>
2024-01-27 17:54:18 +0000

    debug panel update for 1d and planet

--- Source/UI/DebugPanel.h
+++ Source/UI/DebugPanel.h
@@ -10,16 +10,18 @@ namespace UI {
 
 	class DebugPanel : public UIPanel {
 	public:
-		DebugPanel(const Shader& _shader)
-			: UIPanel("Debug", _shader) {
+		DebugPanel(const Shader& _shader, CameraController& _cam_controller)
+			: UIPanel("Debug", _shader), cam_controller_(_cam_controller) {
 			render_target_property_ =
 				std::make_shared<SingleSelectProperty>("Render Target", "iDebugRenderTarget", 0,
 					std::vector<std::string>{
-				"Default",
+					"Default",
 					"Noise2D",
 					"Depth",
 					"OpticalDepth",
-					"Noise3D"
+					"Noise3D",
+					"Noise1D",
+					"Sphere"
 			});
 			properties_ = { 
 				render_target_property_
@@ -29,9 +31,14 @@ namespace UI {
 		void gui() override;
 
 	private:
+		CameraController& cam_controller_;
 		std::shared_ptr<SingleSelectProperty> render_target_property_;
 		bool mark_not_in_atmosphere = false;
 		float noise3D_z_ = 0.0f;
 		float max_distance_ = 10000.0f;
+		float line_height_ = 0.1f;
+		float noise1D_z_ = 0.0f;
+		float sphere_radius_ = 100.0f;
+		float triplanar_sharpness_ = 1;
 	};
 }
